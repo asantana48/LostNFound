a0c8adc6fd5ef14ea594c87d8bef996c274fc338
asantana48 <andres.santana048@gmail.com>
2017-04-30 06:59:34 -0400

    Added animation to lion

--- app/src/main/java/com/santanawilliams/lostandfound/MainActivity.java
+++ app/src/main/java/com/santanawilliams/lostandfound/MainActivity.java
@@ -2,25 +2,45 @@ package com.santanawilliams.lostandfound;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    AlphaAnimation lionAnim, reverseAnim;
     private SoundManager sm;
+    private ImageView lionPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sm = new SoundManager(this);
+        lionPic = (ImageView) findViewById(R.id.blueLionImg);
+
+        lionAnim = new AlphaAnimation(1.0f, 0.0f);
+        reverseAnim = new AlphaAnimation(0.0f, 1.0f);
+        lionAnim.setDuration(2000);
+        reverseAnim.setDuration(2000);
+        lionAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                lionPic.startAnimation(reverseAnim);
+            }
+            @Override
+            public void onAnimationStart(Animation animation) {}
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
 
         //Listener for Nittany Lion Picture
-        final ImageView lionPic = (ImageView) findViewById(R.id.lionImg);
         lionPic.setOnLongClickListener(new View.OnLongClickListener(){
             public boolean onLongClick(View v){
-                lionPic.setImageResource(R.mipmap.psu_lion_red);
+                lionPic.startAnimation(lionAnim);
                 sm.playRoar();
                 return true;
             }
