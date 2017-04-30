f3b285ab30a75123ef3e53f265ddaf5ab2c4d564
asantana48 <andres.santana048@gmail.com>
2017-04-29 20:06:03 -0400

    Database interfacing changes
    Item.java - made Type a regular string because the types will be in the
    array anyway
    strings.xml - added array containing types
    InventoryAdd.java -  connected components, and added an adapter to
    spinner

--- app/src/main/java/com/santanawilliams/lostandfound/InventoryAdd.java
+++ app/src/main/java/com/santanawilliams/lostandfound/InventoryAdd.java
@@ -2,12 +2,39 @@ package com.santanawilliams.lostandfound;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class InventoryAdd extends Activity {
+    private EditText nameET;
+    private EditText contactET;
+    private EditText descriptionET;
+    private Spinner typeSP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_add);
+        initComponents();
+
+        // Set adapter
+        ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(
+                this, R.array.types, android.R.layout.simple_spinner_item);
+
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSP.setAdapter(typeAdapter);
+    }
+
+    private void initComponents() {
+        nameET = (EditText) findViewById(R.id.nameInput);
+        contactET = (EditText) findViewById(R.id.contactInput);
+        descriptionET = (EditText) findViewById(R.id.descriptionInput);
+        typeSP = (Spinner) findViewById(R.id.typeSpinner);
+    }
+
+    public void onAdd(View v) {
+
     }
 }
--- app/src/main/java/com/santanawilliams/lostandfound/Item.java
+++ app/src/main/java/com/santanawilliams/lostandfound/Item.java
@@ -5,39 +5,21 @@ package com.santanawilliams.lostandfound;
  */
 
 public class Item {
-
-    public enum ItemType {
-        SUPPLIES("Office Supplies"),
-        ELECTRONICS ("Electronics"),
-        CLOTHING("Clothing"),
-        JEWELRY("Jewelry"),
-        MISC("Miscellaneous");
-
-        private final String strValue;
-        ItemType(String t) {
-            strValue = t;
-        }
-        @Override
-        public String toString() {
-            return strValue;
-        }
-    }
-
     private int id;
     private String name;
-    private ItemType type;
+    private String type;
     private String contactInfo;
     private String description;
 
     public Item() {
         id = 0;
         name = "NULL";
-        type = ItemType.MISC;
+        type = "NULL";
         contactInfo = "NULL";
         description = "NULL";
     }
 
-    public Item(int i, String n, ItemType t, String c, String d) {
+    public Item(int i, String n, String t, String c, String d) {
         id = i;
         name = n;
         type = t;
@@ -47,13 +29,13 @@ public class Item {
 
     public void setID(int i) { id = i; }
     public void setName(String s) { name = s;}
-    public void setType(ItemType t) { type = t;}
+    public void setType(String t) { type = t;}
     public void setContactInfo(String s) { contactInfo = s;}
     public void setDescription(String s) { description = s;}
 
     public int getId() { return id; }
     public String getName() { return name; }
-    public ItemType getType() { return type; }
+    public String getType() { return type; }
     public String getContactInfo() { return contactInfo; }
     public String getDescription() { return description; }
 }
